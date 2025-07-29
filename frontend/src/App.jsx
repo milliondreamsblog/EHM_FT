a01b0d8ef0d6f2ab15ce6a737518b2de7b3daec1
sanskar <sanskarvns19@gmail.com>
2025-07-29 09:02:12 +0530

    Resolved merge conflicts from stash pop

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,25 @@
+<<<<<<< Updated upstream
 import React from 'react';
 import HeroSection from './Components/Herosection/HeroSection';
+=======
+
+import './App.css'
+import Footer from './Components/Footer/Footer.jsx'
+>>>>>>> Stashed changes
 
 function App() {
   return (
+<<<<<<< Updated upstream
     <div className="App">
       <HeroSection />
     </div>
   );
+=======
+    <>
+<Footer/>
+    </>
+  )
+>>>>>>> Stashed changes
 }
 
 export default App;
