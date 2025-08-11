7c459521ff9881b7a873fbeae419f507715ca5c1
milliondreamsblog <akshatsan23@gmail.com>
2025-08-11 10:12:50 +0530

    make render methods to cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -21,7 +21,10 @@ const app = express();
 // CORS
 app.use(
   cors({
-    origin: ['http://localhost:5173', 'https://ehmconsultancy.co.in','https://ehm-ft.onrender.com'],
+    origin: ['http://localhost:5173', 'https://ehmconsultancy.co.in', 'https://www.ehmconsultancy.co.in', 'https://ehm-ft.onrender.com'],
+    methods: ['GET', 'POST', 'OPTIONS'], // Explicitly allow OPTIONS
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true, // If using cookies or auth
   })
 );
 
