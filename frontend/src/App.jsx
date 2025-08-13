ff0727299eb0a857989c0ba8a1034d7aa1b543ae
UtkarshSharma-004 <utkarsh.ai.developer@gmail.com>
2025-08-13 23:12:26 +0530

    Make the articles page and product page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import "./App.css";
 import Footer from "./Components/Footer/Footer.jsx";
 
@@ -11,14 +12,19 @@ import AdminLoginModal from "./Components/Admin/AdminLoginModal.jsx";
 import AdminRoute from "./Components/Admin/AdminRoute.jsx";
 import Layout from "./Layout.jsx";
 
-
-
-// Updated: Using ArticlesPage instead of ArticlePage
+import ProductPage from "./Pages/Products.jsx";
+import ServicePage from "./Pages/Services.jsx";
 import ArticlesPage from "./Pages/ArticlesPage.jsx";
 import ProductsPage from "./Pages/ProductsPage.jsx";
 
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 function App() {
+  useEffect(() => {
+    AOS.init({ duration: 800, once: true });
+  }, []);
+
   return (
     <BrowserRouter>
       <Layout>
@@ -27,10 +33,12 @@ function App() {
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<ContactPage />} />
           <Route path="/articles" element={<ArticlesPage />} />
+          <Route path="/offerings/products" element={<ProductPage />} />
+          <Route path="/offerings/services" element={<ServicePage />} />
+          <Route path="/products" element={<ProductsPage />} />
 
-            <Route path="/products" element={<ProductsPage />} />
-
-             <Route path="*" element={<HomePage />} />
+          {/* Fallback Home */}
+          <Route path="*" element={<HomePage />} />
 
           {/* Admin Pages */}
           <Route path="/admin/login" element={<AdminLoginModal />} />
