1db79376500b29b0cc8fd72e6dfd995a97b983e5
sitanshukumar173 <sitanshukumar218110@gmail.com>
2025-08-24 18:53:50 +0530

    about section

--- frontend/src/Data/Data.js
+++ frontend/src/Data/Data.js
@@ -1,55 +1,158 @@
+export const companies = [
+  "img1",
+  "img2",
+  "img3",
+  "img7",
+  "img8",
+  "img9",
+  "img10",
+];
 
-export const companies = [ "img1", "img2", "img3", "img7", "img8", "img9", "img10"];
+import one from "/Team/one.png";
+import two from "/Team/two.png";
+import three from "/Team/three.png";
 
-import one from '/Team/one.png';
-import two from '/Team/two.png';
-import three from '/Team/three.png';
+const members = [
+  {
+    name: "Harshit Mishra",
+    title: "Founder & CEO",
+    degree: "Ph.D., IIT Bombay",
+    img: one,
+    social: "https://www.linkedin.com/in/harshitcese/",
+  },
+  {
+    name: "Neha Shukla",
+    title: "Director & COO",
+    degree: "M.Sc., PGDCA",
+    img: two,
+    social: "https://www.linkedin.com/in/neha-shukla-307b0b5b/",
+  },
+  {
+    name: "Utsav Mishra",
+    title: "Co-Founder & CTO",
+    degree: "Ph.D., IIT Kanpur",
+    img: three,
+    social: "https://www.linkedin.com/in/utsav-mishra-phd-08428469/",
+  },
+  {
+    name: "Dr. Jitendre Singh",
+    title: "",
+    degree: "PhD IITB",
+    img: "/Team/Dr-Jitendre-Singh.jpg",
+    social: "https://www.linkedin.com/in/jitendra-singh-ba60325b/",
+  },
+  {
+    name: "Dr. Tarul Sharma",
+    title: "",
+    degree: "PhD IITB",
+    img: "/Team/Dr-Tarul-Sharma.png",
+    social: "https://www.linkedin.com/in/dr-tarul-sharma-36bb6953/",
+  },
+  {
+    name: "Dr. Ramswarup Bhaskar",
+    title: "",
+    degree: "PhD IITK",
+    img: "/Team/Dr-Ramswarup-Bhaskar.png",
+    social: "https://www.linkedin.com/in/rbhaskariitk/",
+  },
+  {
+    name: "Vikas Dohare",
+    title: "Lead Outreach",
+    degree: "M.Sc. IITB",
+    img: "/Team/Vikas-Dohare.png",
+    social: "https://www.linkedin.com/in/vikas-dohare-647301201/",
+  },
+];
 
-   const members = [  
-      {
-      name: "Harshit Mishra",
-        title: "Founder & CEO",
-        degree: "Ph.D., IIT Bombay",
-        img: one,
-        social:"https://www.linkedin.com/in/harshitcese/",
-      },
-      {
-        name: "Neha Shukla",
-        title: "Director & COO",
-        degree: "M.Sc., PGDCA",
-        img: two,
-        social:"https://www.linkedin.com/in/neha-shukla-307b0b5b/",
-      },
-      {
-        name: "Utsav Mishra",
-        title: "Co-Founder & CTO",
-        degree: "Ph.D., IIT Kanpur",
-        img: three,
-        social:"https://www.linkedin.com/in/utsav-mishra-phd-08428469/"
-      }
-    ];
+const advisors = [
+  {
+    name: "Mr. Arks Srinivas",
+    title: "CEO, IFACET IIT Kanpur",
+    degree: "MBA (Marketing and Systems), IIM Calcutta",
+    img: "/Team/image-2.png",
+    social:
+      "https://www.linkedin.com/in/arks-srinivas-akkapeddi-907b2b3/?originalSubdomain=in",
+  },
+  {
+    name: "Mr. Vivek Mishra",
+    title: "CEO,CSJMIF,CSJMU Kanpur B.Tech(Electrical Engineering)",
+    degree: " HBTU Kanpur",
+    img: "/Team/Vivek-Sir.jpeg",
+    social: "",
+  },
+  {
+    name: "Mr. Anil Gupta",
+    title: "Sigma International Inc",
+    degree: "Director",
+    img: "/Team/Anil-Gupta.jpeg",
+    social: "",
+  },
+  {
+    name: "Prof. S. Karmakar",
+    title: "Climate Risk",
+    degree: "IIT Bombay",
+    img: "/Team/Prof-S-Karmakar.png",
+    social: "",
+  },
+  {
+    name: "Prof. B.V. Phani",
+    title: "Corporate Finance",
+    degree: "IIT Kanpur",
+    img: "/Team/Prof-BV-Phani.png",
+    social: "",
+  },
+  {
+    name: "Prof. Tarun Gupta",
+    title: "Environmental Engineering",
+    degree: "IIT Kanpur",
+    img: "/Team/Prof-Tarun-Gupta.png",
+    social: "",
+  },
+  {
+    name: "Prof. Animesh Mandal",
+    title: "Exploration Geophysics",
+    degree: "IIT Kanpur",
+    img: "/Team/Prof-Animesh-Mandal.png",
+    social: "",
+  },
+];
 
+const experts = [
+  {
+    name: "Dr. Anand Hiremath",
+    title: "IRClass & Head SSORP",
+    degree: "Sustainability & ESG",
+    img: "/Team/anand.png",
+    social: "",
+  },
+  {
+    name: "Er. Prabhjot Singh",
+    title: "Director PD&EX management services",
+    degree: "Structural Engineering",
+    img: "/Team/prabhjot.png",
+    social: "",
+  },
+  {
+    name: "Ms. Katyayani Mishra",
+    title: "Director & CEO",
+    degree: "Centre for Policy research and legal initiatives",
+    img: "/Team/katyayani.png",
+    social: "",
+  },
+  {
+    name: "Mr. Ramanuj Mitra",
+    title: "Senior Consultant Sustainability",
+    degree: "M.S. in Sustainability, Arizona State University",
+    img: "/Team/ramanuj.png",
+    social: "",
+  },
+  {
+    name: "Dr. Roshan Singh",
+    title: "Asst. Professor, Dept of Petroleum Engineering and Earth Sciences",
+    degree: "IIPE Visakhapatnam",
+    img: "/Team/roshan.png",
+    social: "",
+  },
+];
 
-    const advisors = [
-      {
-        name: "Mr. Arks Srinivas",
-        title: "CEO, IFACET IIT Kanpur",
-        degree: "MBA (Marketing and Systems), IIM Calcutta",
-        img: "/Team/image-2.png",
-        social:"https://www.linkedin.com/in/arks-srinivas-akkapeddi-907b2b3/?originalSubdomain=in"
-      },
-      {
-         name: "Mr. Vivek Mishra",
-        title: "CEO,CSJMIF,CSJMU Kanpur B.Tech(Electrical Engineering)",
-        degree: " HBTU Kanpur",
-        img: "/Team/Vivek-Sir.jpeg",
-      },
-      {
-         name: "Mr. Anil Gupta",
-        title: "Sigma International Inc",
-        degree: "Director",
-        img: "/Team/Anil-Gupta.jpeg",
-      }
-    ];
-
-    export {members , advisors};
+export { members, advisors, experts };
