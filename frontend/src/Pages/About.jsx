54cc07c1488cb22152e108c08d7341545e00fd82
sitanshukumar173 <sitanshukumar218110@gmail.com>
2025-09-06 20:02:47 +0530

    added the why choose us section

--- frontend/src/Pages/About.jsx
+++ frontend/src/Pages/About.jsx
@@ -4,12 +4,14 @@ import Team from '../Components/LandingPage/Team'
 import { members, advisors, experts } from '../Data/Data'
 import Hero from '../Components/About/Hero'
 import ChooseUs from '../Components/About/ChooseUS'
+import WhyChooseUs from '../Components/About/WhyChooseUs'
 
 const About = () => {
   return (
     <div className="min-h-screen bg-gradient-to-br from-emerald-50 via-green-50 to-teal-50 ">
       <Hero />
 
+      <WhyChooseUs />
 
       <Team title="Our Team" members={members} />
 
