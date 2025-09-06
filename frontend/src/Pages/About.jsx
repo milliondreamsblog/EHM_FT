b52e07d94c7efee7e378dc78c4e8357f60a21dc8
Ayush <Ayushverma23904@gmail.com>
2025-09-06 23:46:14 +0530

    apdated about section

--- frontend/src/Pages/About.jsx
+++ frontend/src/Pages/About.jsx
@@ -4,12 +4,14 @@ import Team from '../Components/LandingPage/Team'
 import { members, advisors, experts } from '../Data/Data'
 import Hero from '../Components/About/Hero'
 import ChooseUs from '../Components/About/ChooseUS'
+import Description from '../Components/About/Description'
 
 const About = () => {
   return (
     <div className="min-h-screen bg-gradient-to-br from-emerald-50 via-green-50 to-teal-50 ">
       <Hero />
-
+       
+       <Description />
 
       <Team title="Our Team" members={members} />
 
