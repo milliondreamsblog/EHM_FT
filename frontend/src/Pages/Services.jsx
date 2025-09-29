5bf520a61f574398866ba9d81627ea10eaa16bba
UtkarshSharma-004 <utkarsh.ai.developer@gmail.com>
2025-09-30 01:39:37 +0530

    Made new Section in offerings,Update webinaar

--- frontend/src/Pages/Services.jsx
+++ frontend/src/Pages/Services.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import ServicesSection from "../Components/LandingPage/ServiceSection";
+import RiskAssessment from "../Components/Risk Assessment/RiskAssessment";
+import RiskQuestionnaire from "../Components/Risk Assessment/RiskQuestionnaire";
+import HelpSection from "../Components/HelpSection";
 
 const ServicePage = () => {
     return(
         <div className="min-h-screen bg-gradient-to-br from-green-50 to-teal-50"> 
             <ServicesSection/>
+            <RiskAssessment/>
+            <RiskQuestionnaire/>
+            <HelpSection/>
             
         </div>
     )
